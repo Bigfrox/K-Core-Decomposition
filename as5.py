dc08c29d30412da3d24a8ad75963040eabcce140
Bigfrox <bigfroxgx@gmail.com>
2021-11-16 01:57:11 +0900

    11.16

--- as5.py
+++ as5.py
@@ -2,11 +2,10 @@
 Data Mining
 Assignment 5, Graph Clustering by k-Core Decomposition
 Myeong suhwan
-
 '''
 
 from datetime import datetime
-import time
+import sys
 
 def getDataFromFile(filename):
     input_file = open(filename,'r')
@@ -63,50 +62,60 @@ def GetNeighbors(graph,vertex,node_set):
 
 
 def main():
-    inputfile = 'assignment5_input.txt' #! sys
+    #inputfile = 'assignment5_input.txt' #! sys
     #inputfile = 'test_input.txt'
+
+
+    if len(sys.argv) != 2:
+        print("No input file.")
+        print("<Usage> as5.py assignment5_input.txt")
+        return -1;
+    
+    inputfile = sys.argv[1]
     output_filename = 'output.txt'
+
     graph = getDataFromFile(inputfile)
     start_time = datetime.now()
     
     k=2
-    isChanged = True
 
+    isChanged = True
     while isChanged:
         isChanged = False
         del_list = []    
         
         for v in graph:
-            #print(v ," ::", graph[v])
+            
             if len(graph[v]) <= k:
                 del_list.append(v)
                 isChanged = True
         for d in del_list:
-            #print(d)
+            
             del graph[d] #* delete vertex
 
         for d in del_list:
             for v in graph:
                 if d in graph[v]:
-                    print(d," is deleted in ", graph[v])
+                    #print(d," is deleted in ", graph[v])
                     graph[v].remove(d) #* delete edge
-                    print("result = ", graph[v])
+                    #print("result = ", graph[v])
                     
 
 
     graph = dict(sorted(graph.items(),key=lambda x : len(x[1]), reverse=True))
     
-    for v in graph:
-        print(v ," ::", len(graph[v]),graph[v]) #* Degree
- 
+    #* Show degree of every node
+    # for v in graph:
+    #     print(v ," ::", len(graph[v]),graph[v])
+
     cluster = list()
     
-    print("Graph is made")
+    #* first of all, cluster with vertex and neighbors
     for v in graph:
         node_set = set()
         node_set.add(v) #*(1)
         node_set = GetNeighbors(graph,vertex=v,node_set=node_set) #*(2)
-        #print(node_set)
+        
         cluster.append(node_set)
 
 
@@ -117,11 +126,10 @@ def main():
 
     while index < len(cluster):
         v = cluster[index]
-        #print("=====================")
-        #print("v : ", v)
+        
         j_index = 0
         while j_index < len(cluster):
-            #print("j_index",j_index)
+            
             
             if index == j_index:
                 j_index += 1
@@ -130,9 +138,9 @@ def main():
                 break    
 
             u = cluster[j_index]
-            #print("u : ", u)
+            
             if v == u:
-                #print("v:", v)
+                
                 #print(u," is deleted")
                 cluster.remove(u)
                 j_index -= 1
@@ -142,17 +150,8 @@ def main():
                 
 
 
-     
-    i=0
-    for v in cluster:
-        if not v:
-            continue
-        print("cluster",i,v)
-        print("\n\n")
-        i +=1   
-    
 
-    #* Clustering
+    #* Merge two clusters if common neighbor is.
     isChanged = True
     while(isChanged):
         isChanged = False
@@ -169,15 +168,16 @@ def main():
                     continue
                 
                 if cluster[i].intersection(cluster[j]):
-                    print(" = = = = = = == = == = == = = ")
+                    # print(" = = = = = = == = == = == = = ")
                     # print(len(cluster[i]),"cluster i: ",cluster[i])
                     # print(len(cluster[j]),"cluster j: ",cluster[j])
-                    print("intersection : ",cluster[i].intersection(cluster[j]))
+                    # print("intersection : ",cluster[i].intersection(cluster[j]))
                     cluster[i] = cluster[i].union(cluster[j])
                     cluster[j] = set()
                     isChanged = True
                     # print(len(cluster[i]),"union=>>",cluster[i])
     
+    #* show cluster number and nodes in the cluster
     i=0
     for v in cluster:
         if not v:
@@ -186,79 +186,9 @@ def main():
         print("\n\n")
         i +=1   
 
-
-
-    # new_cluster = list()
-    # redundant_set = set()
-    # is_Changed = True
-
-    
-    # num = 0
-
-    # while is_Changed:
-    #     num += 1
-    #     print("num : ",num)
-    #     is_Changed = False
-
-    #     index = 0
-    #     while index < len(cluster):
-    #         v = cluster[index]
-    #         result = set()
-    #         j_index = 0
-    #         if index in redundant_set:
-    #             index += 1
-    #             continue
-    #         while j_index < len(cluster):
-    #             #print("j" ,j_index)
-    #             if index == j_index:
-    #                 j_index += 1
-    #                 continue
-    #             if j_index >= len(cluster):
-    #                 break
-    #             if j_index in redundant_set:
-    #                 j_index += 1
-    #                 continue
-
-    #             u = cluster[j_index]
-    #             #print("u : ", u)
-    #             if v.intersection(u):
-    #                 #print("intersection : ",v.intersection(u))
-                    
-    #                 is_Changed = True
-    #                 result = result.union(v.union(u))
-                        
-                    
-    #                 redundant_set.add(j_index)
-                    
-            
-    #             j_index += 1
-    #         #print(result)
-    #         new_cluster.append(result)
-    #         index += 1
-
-
-
-
-
-
-
-
-    # i=0
-    # for v in new_cluster:
-    #     if not v:
-    #         continue
-    #     print("cluster",i,v)
-    #     print("\n\n")
-    #     i +=1
-
-
-
-
     print("[+] Time Elapsed : ", datetime.now() - start_time, "microseconds")
 
-
-
-
+    #* save to output file with size : nodes in a cluster
     output_to_file(output_filename,cluster)
     
 
